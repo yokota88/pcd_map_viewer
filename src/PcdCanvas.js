278b6e59eed4a71dcc99850f89d8088b9c78e6d7
tadayuki yokota <t.yokota.eng@gmail.com>
2022-09-25 21:02:12 +0900

    Add pcd data range calculation

--- src/PcdCanvas.js
+++ src/PcdCanvas.js
@@ -11,18 +11,40 @@ import { useContext } from "react";
 import { PcdFilePropsContext } from "./components/providers/PcdFilePropsProvider";
 import { usePCDLoader } from "./loader";
 
+const calcMinMax = (target) => {
+    let i;
+    let min;
+    let max;
+    i = target.length;
+    min = Infinity;
+    max = -Infinity;
+    while (i--) {
+        if (target[i] < min) { min = target[i] }
+        if (target[i] > max) { max = target[i] }
+    }
+
+    return {max:max, min:min};
+}
 
 const Model = (props) => {
-    let result = usePCDLoader(props.file_path);
-    if(result != null){
-        result.material.size = 0.15;
+    let points = usePCDLoader(props.file_path);
+    console.log(points)
+    if(points != null){
+        const itemSize = points.geometry.attributes.position.itemSize;
+        const points_x = points.geometry.attributes.position.array.filter((val,idx)=>idx%itemSize===0);
+        const points_y = points.geometry.attributes.position.array.filter((val,idx)=>idx%itemSize===1);
+        const points_z = points.geometry.attributes.position.array.filter((val,idx)=>idx%itemSize===2);
+        const range_x = calcMinMax(points_x);
+        const range_y = calcMinMax(points_y);
+        const range_z = calcMinMax(points_z);
+        points.material.size = 0.15;
     }else{
-        result = new Points();
+        points = new Points();
     }
 
     return (
         <Suspense fallback={null}>
-          <primitive object={result} />
+          <primitive object={points} />
         </Suspense>
       )
 };
